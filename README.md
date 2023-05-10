ae4f128ebbdc9f18b902db5d391d546d496e7cfd
benhuangbmj <120871839+benhuangbmj@users.noreply.github.com>
2023-05-10 04:29:28 +0000

    Created "add" button

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-<base target="_blank">
-
 # Instructure Canvas API Utilities
 ## Description
   Canvas by Instructure is a widely adopted Learning Management System (LMS) across the US. It offers a REST API to the educators who want to create their own ways to solve problems. The project utilizes this API to extend the functionality of Canvas.
--- view/components/service_list.js
+++ view/components/service_list.js
@@ -1,6 +1,55 @@
 export default function(populate = false) {
-  const services = ['Check-Overdue', "Sync-Grades", "Service-3"];//This array holds the title of each service.
-  const reference = [false, true, false];// Indicate whether the service references a source and a target.
+  const services = ['Check-Overdue', "Sync-Grades", "Assign-Grades"];//This array holds the title of each service.
+  const types = [1, 2, 3];// Indicate whether the service references a source and a target.
+  const listCourses = function(res, courseCell, courseDiv, service) {
+    for (let i = 0; i < res.length; i++) {
+      const serviceId = service;
+      const id = serviceId + '-' + res[i].course_code.split(' ').join('-');
+      const label = $('<label></label>');
+      label
+        .text(`${res[i].name} (${res[i].course_code}, ${res[i].id})`)
+        .attr('for', `${id}`)
+        .css('margin-left', '30px')
+        .css('display', 'inline-block');
+      courseDiv.append(
+        $('<div></div>')
+          .addClass('checkbox')
+          .addClass(`${res[i].id}`)
+          .append(
+            $('<input>')
+              .attr('id', `${id}`)
+              .attr('type', 'checkbox')
+              .attr('name', `${serviceId}`)
+              .attr('value', `${res[i].id}`)
+              .css('display', 'inline-block')
+              .css('margin-left', '20px')
+          )
+          .append(label)
+      );
+    };
+    courseCell.attr("colspan", "2");
+  }
+  const sourceTarget = function(res, courseCell, targetCell, courseDiv, targetDiv, service, id, row) {              
+              const serviceId = service;
+              const selectTarget = $("<select></select>");
+              const selectSource = $("<select></select>");
+              selectTarget.attr('name', `${serviceId + "-target"}`);
+              targetDiv.append(selectTarget);
+              selectSource.attr('name', `${serviceId + "-source"}`);
+              courseDiv.append(selectSource);
+              selectSource.append($(`<option value = null>Select a course</option>`));
+              selectTarget.append($(`<option value = null>Select a course</option>`))
+              for (let i = 0; i < res.length; i++) {
+                const option = Array.from(Array(2), () => $("<option></option>"));
+                option.forEach(e => {
+                  e.attr("value", `${res[i].id}`)
+                    .text(`${res[i].name} (${res[i].course_code}, ${res[i].id})`);
+                });
+                selectSource.append(option[0]);
+                selectTarget.append(option[1]);
+              }
+  }
+  
   if (!populate) {
     const form = $('<form action="/subscribe" method="post" class="invisible"></form>');
     const table = $('<table class="table table-bordered table-striped table-hover"><tr><th>Service</th><th>Subscribe</th><th colspan = "2">Courses</th></tr></table>');//Define the service list table headers.
@@ -31,59 +80,26 @@ export default function(populate = false) {
               $(`<td id="${id}-2"></td>`)
                 .html(`<input type="checkbox" name="${id}-sub" style="display: block; margin: auto">`) // Define the subsribe checkbox cell
             )
-            const courseCell = $(`<td id=${id}-3></td>`);
-            if(reference[j] == true) {
-              courseCell.append('<th>Source</th>');
-            }
+            const courseCell = $(`<td id=${id}-3></td>`);//Create the cource cell.            
             const courseDiv = $('<div class="container-fluid"></div>');
-            courseCell.append(courseDiv);
-            row.append(courseCell);//Create the cource cell.
-            for (let i = 0; i < res.length; i++) {
-              const serviceId = services[j];
-              const id = serviceId + '-' + res[i].course_code.split(' ').join('-');
-              const label = $('<label></label>');
-              label
-                .text(`${res[i].name} (${res[i].course_code}, ${res[i].id})`)
-                .attr('for', `${id}`)
-                .css('margin-left', '30px')
-                .css('display', 'inline-block');
-              courseDiv.append(
-                $('<div></div>')
-                  .addClass('checkbox')
-                  .addClass(`${res[i].id}`)
-                  .append(
-                    $('<input>')
-                      .attr('id', `${id}`)
-                      .attr('type', 'checkbox')
-                      .attr('name', `${serviceId}`)
-                      .attr('value', `${res[i].id}`)
-                      .css('display', 'inline-block')
-                      .css('margin-left', '20px')
-                  )
-                  .append(label)
-              );
-            };
-            if(reference[j] == true) {//Create the "target" cell.
-              const courseCell = $(`<td id=${id}-4></td>`);
-              courseCell.append('<th>Target</th>');
-              const courseDiv = $('<div class="container-fluid"></div>');
-              courseCell.append(courseDiv);
-              row.append(courseCell);
-              const serviceId = services[j] + "-target";
-              const select = $("<select></select>");
-              select.attr('name', `${serviceId}`);
-              courseDiv.append(select);
-              for (let i = 0; i < res.length; i++) {                
-                const id = serviceId + '-' + res[i].course_code.split(' ').join('-') + "-target";
-                const option = $("<option></option>");
-                option
-                  .attr("value", `${res[i].id}`)
-                  .text(`${res[i].name} (${res[i].course_code}, ${res[i].id})`);
-                select.append(option);              
-              };
-            } else {
-              $(`#${id}-3`).attr("colspan", "2");
+            row.append(courseCell);
+            if (types[j] == 1) {
+              listCourses(res, courseCell, courseDiv, services[j]);
+            } else if (types[j] == 2) {//Create the cross-reference cell.
+              const targetCell = $(`<td id=${id}-4></td>`);
+              targetCell.append('<th>Target</th>');
+              courseCell.append('<th>Source</th>');
+              const targetDiv = $('<div class="container-fluid"></div>');
+              targetCell.append(targetDiv);
+              row.append(targetCell);
+              sourceTarget(res, courseCell, targetCell, courseDiv, targetDiv, services[j], id, row);
+              const addButton = $("<button type = 'button' class='btn btn-primary'>Add</button>");
+              addButton.on('click', function() {
+                sourceTarget(res, courseCell, targetCell, courseDiv, targetDiv, services[j], id, row);
+              });
+              targetCell.append(addButton);
             }
+            courseCell.append(courseDiv);
             const deliver = $(`<button type='submit' class='btn btn-primary' formaction='/${id}'>Deliver</button>`);//Use the "formaction" attribute to change the default action of the submit button.
             courseCell.append(deliver);
           };
@@ -95,7 +111,7 @@ export default function(populate = false) {
   1. Add "source" and "target" table header to the rows accordingly.
   2. Merge the cells where there is no need for cross reference.
   3. Change the checkbox input for cross reference service to dropdown menu. Introduce the 1-1 correspondence.
-  */  
+  */
   //Start populating from the database
   const form = $('form');
   const table = $('table');
