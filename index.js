e02a65d773c93e317b6abe7a66324b29f7f7cba0
Ben Huang <benjaminwong1985@gmail.com>
2024-05-13 00:13:40 -0400

    chore: format the code

--- index.js
+++ index.js
@@ -1,186 +1,214 @@
-const express = require('express');
-const session = require('express-session')
-const path = require('path');
-const bodyParser = require('body-parser');
-require('dotenv').config();
-const urlPrefixTest = 'https://canvas.test.instructure.com'
-const myToken = process.env['CANVAS_API_TOKEN_TEST'];
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const urlPrefixTest = "https://canvas.test.instructure.com";
+const myToken = process.env["CANVAS_API_TOKEN_TEST"];
 const headersTest = {
   Authorization: `Bearer ${myToken}`,
-  Accept: "application/json+canvas-string-ids"
-}
+  Accept: "application/json+canvas-string-ids",
+};
 const app = express();
 const port = process.env.PORT || 3000;
-const staticPath = path.join(__dirname, 'view');
-const homepage = path.join(__dirname, 'view/index.html');
-const profilePage = path.join(__dirname, 'view', 'profile.html');
-const sessionSecret = process.env['SESSION_SECRET'];
-const courseIdForTest = process.env['COURSE_ID'];
-const errorHandler = function(res, error) {
+const staticPath = path.join(__dirname, "view");
+const homepage = path.join(__dirname, "view/index.html");
+const profilePage = path.join(__dirname, "view", "profile.html");
+const sessionSecret = process.env["SESSION_SECRET"];
+const courseIdForTest = process.env["COURSE_ID"];
+const errorHandler = function (res, error) {
   res.send(error);
-}
-const makeSafe = function(fn, req, res, processor, errorHandler) {
-  return function() {
+};
+const makeSafe = function (fn, req, res, processor, errorHandler) {
+  return function () {
     fn()
-      .then(response => {
-      processor(req, res, response);
+      .then((response) => {
+        processor(req, res, response);
       })
-      .catch(error => {
+      .catch((error) => {
         errorHandler(res, error);
       });
-  }
-}
+  };
+};
 app.listen(port, () => {
   console.log(`The server is listening at port ${port}.`);
 });
 
 app.use((req, res, next) => {
-  console.log((new Date()).toLocaleString('en-US', {timeZone: "America/New_York"}), req.method, req.path);
+  console.log(
+    new Date().toLocaleString("en-US", { timeZone: "America/New_York" }),
+    req.method,
+    req.path,
+  );
   next();
-})
+});
 app.use(express.static(staticPath));
 
-app.use(session({
-  secret: sessionSecret,
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    secure: false, //how to make this work?
-    httpOnly: false
-  }
-}));
+app.use(
+  session({
+    secret: sessionSecret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: false, //how to make this work?
+      httpOnly: false,
+    },
+  }),
+);
 
 app.use(bodyParser.text());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.route('/').get((req, res) => res.sendFile(homepage));
+app.route("/").get((req, res) => res.sendFile(homepage));
 
-app.route('/pass').post((req, res) => res.json(process.env['CANVAS_API_TOKEN_TEST']));
+app
+  .route("/pass")
+  .post((req, res) => res.json(process.env["CANVAS_API_TOKEN_TEST"]));
 
-app.route('/profile')
+app
+  .route("/profile")
   .post((req, res) => {
     req.session.headers = {
       Authorization: `Bearer ${req.body.token}`,
-      Accept: "application/json+canvas-string-ids"
+      Accept: "application/json+canvas-string-ids",
     };
     req.session.token = req.body.token;
     req.session.envir = req.body.envir;
-    if (req.body.envir == 'production') {
-      req.session.urlPrefix = 'https://canvas.instructure.com';
+    if (req.body.envir == "production") {
+      req.session.urlPrefix = "https://canvas.instructure.com";
     } else {
-      req.session.urlPrefix = 'https://canvas.' + req.body.envir + '.instructure.com';
+      req.session.urlPrefix =
+        "https://canvas." + req.body.envir + ".instructure.com";
     }
     res.sendFile(profilePage);
   })
   .get((req, res) => {
     if (!req.session.token) {
-      res.redirect('/')
+      res.redirect("/");
     } else {
-    res.sendFile(profilePage);
+      res.sendFile(profilePage);
     }
   });
 
-app.route('/user').get((req, res) => {
+app.route("/user").get((req, res) => {
   if (!req.session.token) {
-    return res.redirect('/');
+    return res.redirect("/");
   }
   const session = req.session;
 
-  const getProfile = require('./utilities').getProfile(session);
-  getProfile.then(profile => {
+  const getProfile = require("./utilities").getProfile(session);
+  getProfile.then((profile) => {
     session.userId = profile.id;
     res.json(profile);
   });
 });
 
-app.route('/courses').get((req, res, next) => {
-  const getCourses = require('./utilities').getCourses;
+app.route("/courses").get((req, res, next) => {
+  const getCourses = require("./utilities").getCourses;
   getCourses(req.session.urlPrefix, req.session.headers)
-    .then(response => res.json(response))
-    .catch(err => next(err));
+    .then((response) => res.json(response))
+    .catch((err) => next(err));
+});
 
-})
+app.route("/subscribe").post((req, res) => {
+  require("./utilities")
+    .subscribe(req)
+    .then(() => res.redirect("/profile"));
+});
 
-app.route('/subscribe').post((req, res) => {
-  require('./utilities').subscribe(req)
-    .then(() => res.redirect('/profile'));
-})
+app.route("/Check-Overdue").post((req, res) => {
+  const courses = Array.isArray(req.body["Check-Overdue"])
+    ? req.body["Check-Overdue"]
+    : [req.body["Check-Overdue"]];
 
-app.route('/Check-Overdue').post((req, res) => {
-  const courses = Array.isArray(req.body["Check-Overdue"])?req.body["Check-Overdue"]:[req.body["Check-Overdue"]];
-  
-  courses.forEach(course => {
-    require('./utilities').checkOverdue(true, course, req.session.urlPrefix, req.session.headers, 6).then(() => {
-      console.log('Check overdue for ', course);
-    });
-  })
-  res.send("Check-Overdue");
-})
-
-app.route('/Sync-Grades').post((req, res) => {
-  const [source, target] = [req.body["Sync-Grades-source"], req.body["Sync-Grades-target"]];
-  require('./utilities').syncGrades(req.session.urlPrefix, req.session.headers, source, target).then(() => {
-    console.log('Sync Grades for ', source, target);
+  courses.forEach((course) => {
+    require("./utilities")
+      .checkOverdue(true, course, req.session.urlPrefix, req.session.headers, 6)
+      .then(() => {
+        console.log("Check overdue for ", course);
+      });
   });
-  res.send("Sync Grades");
-})
+  res.send("Check-Overdue");
+});
 
-app.route('/groups').post((req, res) => {
-  require('./utilities')
+app.route("/Sync-Grades").post((req, res) => {
+  const [source, target] = [
+    req.body["Sync-Grades-source"],
+    req.body["Sync-Grades-target"],
+  ];
+  require("./utilities")
+    .syncGrades(req.session.urlPrefix, req.session.headers, source, target)
+    .then(() => {
+      console.log("Sync Grades for ", source, target);
+    });
+  res.send("Sync Grades");
+});
+
+app.route("/groups").post((req, res) => {
+  require("./utilities")
     .getAssignmentGroups(req.session.urlPrefix, req.session.headers, req.body)
-    .then(response => res.json(response))
-    .catch(error => {
+    .then((response) => res.json(response))
+    .catch((error) => {
       console.log(error);
     });
 });
 
-app.route('/Assign-Grades').post((req, res) => {
-  const [courseId, groupId] = [req.body['Assign-Grades-courses'], req.body['Assign-Grades-groups']];
-  require('./utilities').assignGrades(req.session.urlPrefix, req.session.headers, courseId, groupId)
-    .then(response => {
-      res.redirect('/profile');
+app.route("/Assign-Grades").post((req, res) => {
+  const [courseId, groupId] = [
+    req.body["Assign-Grades-courses"],
+    req.body["Assign-Grades-groups"],
+  ];
+  require("./utilities")
+    .assignGrades(req.session.urlPrefix, req.session.headers, courseId, groupId)
+    .then((response) => {
+      res.redirect("/profile");
     })
-    .catch(err => {
-      console.log(err)
-      res.redirect('/profile');
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/profile");
     });
-})
+});
 
-app.route('/Create-Peer-Graded').post((req, res) => {
+app.route("/Create-Peer-Graded").post((req, res) => {
   let {
-    'Create-Peer-Graded-course': courseId,
-    'Create-Peer-Graded-name': name,
-    'Create-Peer-Graded-group': group,
+    "Create-Peer-Graded-course": courseId,
+    "Create-Peer-Graded-name": name,
+    "Create-Peer-Graded-group": group,
   } = req.body;
   const assignmentData = {
     assignment: {
       name: name,
       group_name: group,
       points_possible: 5,
-      published: true
-    }
+      published: true,
+    },
   };
-  require('./utilities')
-    .createPeerGradedAssignment(req.session.urlPrefix, req.session.headers, courseId, assignmentData)
-    .then(() => res.redirect('/profile'))
-    .catch(err => {
+  require("./utilities")
+    .createPeerGradedAssignment(
+      req.session.urlPrefix,
+      req.session.headers,
+      courseId,
+      assignmentData,
+    )
+    .then(() => res.redirect("/profile"))
+    .catch((err) => {
       console.log(err);
       res.send(err);
     });
 });
 
-app.route('/Process-Peer-Graded').post((req, res) => {
-  if (Array.isArray(req.body['Process-Peer-Graded'])) {
-    var courseIds = req.body['Process-Peer-Graded'];
+app.route("/Process-Peer-Graded").post((req, res) => {
+  if (Array.isArray(req.body["Process-Peer-Graded"])) {
+    var courseIds = req.body["Process-Peer-Graded"];
   } else {
-    var courseIds = [req.body['Process-Peer-Graded']];
+    var courseIds = [req.body["Process-Peer-Graded"]];
   }
-  for(let i = 0; i < courseIds.length; i++) {
+  for (let i = 0; i < courseIds.length; i++) {
     const courseId = courseIds[i];
     try {
-      require('./src/controllers/processPeerGraded')
+      require("./src/controllers/processPeerGraded")
         .processPeerGraded(req.session.urlPrefix, req.session.headers, courseId)
-        .then(() => res.send('Processed peer graded assignments.'))
+        .then(() => res.send("Processed peer graded assignments."));
     } catch (err) {
       console.log(err);
       res.send(err);
@@ -188,31 +216,35 @@ app.route('/Process-Peer-Graded').post((req, res) => {
   }
 });
 
-app.route('/Sort-Into-Groups').post((req, res) => {
-  if (Array.isArray(req.body['Sort-Into-Groups'])) {
-    var courseIds = req.body['Sort-Into-Groups'];
+app.route("/Sort-Into-Groups").post((req, res) => {
+  if (Array.isArray(req.body["Sort-Into-Groups"])) {
+    var courseIds = req.body["Sort-Into-Groups"];
   } else {
-    var courseIds = [req.body['Sort-Into-Groups']];
+    var courseIds = [req.body["Sort-Into-Groups"]];
   }
-  const helper = function(i) {
+  const helper = function (i) {
     if (i < courseIds.length) {
       try {
-        require('./utilities')
-          .sortIntoGroups(req.session.urlPrefix, req.session.headers, courseIds[i])
+        require("./utilities")
+          .sortIntoGroups(
+            req.session.urlPrefix,
+            req.session.headers,
+            courseIds[i],
+          )
           .then(() => {
-            helper(i+1);
+            helper(i + 1);
           })
-          .catch(err => {
+          .catch((err) => {
             console.log(err);
             res.send(err);
           });
       } catch (err) {
         res.send(err);
-      }    
+      }
     }
-  }
+  };
   helper(0);
-  res.send('Sort into Groups.');
-})
+  res.send("Sort into Groups.");
+});
 
-app.use((req, res) => res.redirect('/'));
+app.use((req, res) => res.redirect("/"));
--- view/components/service_list.js
+++ view/components/service_list.js
@@ -1,34 +1,41 @@
-export default function(mQL, populate = false) {
-  const services = ['Check-Overdue', "Sync-Grades", "Assign-Grades", "Create-Peer-Graded", 'Process-Peer-Graded', 'Sort-Into-Groups'];
-  const types = [1, 2, 3, 4, 1,1];
-  const listCourses = function(res, courseCell, courseDiv, service) {
+export default function (mQL, populate = false) {
+  const services = [
+    "Check-Overdue",
+    "Sync-Grades",
+    "Assign-Grades",
+    "Create-Peer-Graded",
+    "Process-Peer-Graded",
+    "Sort-Into-Groups",
+  ];
+  const types = [1, 2, 3, 4, 1, 1];
+  const listCourses = function (res, courseCell, courseDiv, service) {
     for (let i = 0; i < res.length; i++) {
       const serviceId = service;
-      const id = serviceId + '-' + res[i].course_code.split(' ').join('-');
-      const label = $('<label></label>');
+      const id = serviceId + "-" + res[i].course_code.split(" ").join("-");
+      const label = $("<label></label>");
       label
         .text(`${res[i].name} (${res[i].course_code}, ${res[i].id})`)
-        .attr('for', `${id}`)
-        .css('margin-left', '30px')
-        .css('display', 'inline-block');
+        .attr("for", `${id}`)
+        .css("margin-left", "30px")
+        .css("display", "inline-block");
       courseDiv.append(
-        $('<div></div>')
-          .addClass('checkbox')
+        $("<div></div>")
+          .addClass("checkbox")
           .addClass(`${res[i].id}`)
           .append(
-            $('<input>')
-              .attr('id', `${id}`)
-              .attr('type', 'checkbox')
-              .attr('name', `${serviceId}`)
-              .attr('value', `${res[i].id}`)
-              .css('display', 'inline-block')
-              .css('margin-left', '20px')
+            $("<input>")
+              .attr("id", `${id}`)
+              .attr("type", "checkbox")
+              .attr("name", `${serviceId}`)
+              .attr("value", `${res[i].id}`)
+              .css("display", "inline-block")
+              .css("margin-left", "20px"),
           )
-          .append(label)
+          .append(label),
       );
-    };
+    }
     courseCell.attr("colspan", "2");
-  }
+  };
   /*
   const makeCourseMenu = function(res, div, service, suffix = '') {
     const serviceId = service;
@@ -50,12 +57,12 @@ export default function(mQL, populate = false) {
       ulDropdown.append(li);
     }
     return divDropdown;
-  }*/ //Tried to use the dropdown menu in Bootstrap. Doesn't work so far.   
-  const makeCourseMenu = function(res, div, service, suffix = '') {
+  }*/ //Tried to use the dropdown menu in Bootstrap. Doesn't work so far.
+  const makeCourseMenu = function (res, div, service, suffix = "") {
     const serviceId = service;
     const select = $("<select class='container-fluid'></select>");
-    select.attr('name', `${serviceId + suffix}`);
-    select.attr('id', `${serviceId + suffix}`);
+    select.attr("name", `${serviceId + suffix}`);
+    select.attr("id", `${serviceId + suffix}`);
     div.append(select);
     select.append($(`<option value = null>Select a course</option>`));
     for (let i = 0; i < res.length; i++) {
@@ -66,165 +73,189 @@ export default function(mQL, populate = false) {
       select.append(option);
     }
     return select;
-  }
+  };
   if (mQL.matches) {
     if (!populate) {
-      const form = $('<form action="/subscribe" method="post" class="invisible"></form>'); 
-      const table = $('<table class="table table-bordered table-striped table-hover"><tr><th>Service</th><th>Subscribe</th><th colspan = "2">Courses</th></tr></table>');//Define the service list table headers.
+      const form = $(
+        '<form action="/subscribe" method="post" class="invisible"></form>',
+      );
+      const table = $(
+        '<table class="table table-bordered table-striped table-hover"><tr><th>Service</th><th>Subscribe</th><th colspan = "2">Courses</th></tr></table>',
+      ); //Define the service list table headers.
       form.append(table);
-      $('#service-list').append(form);//#service-list is a <div> element in profile.html.
-  
-      fetch('/courses')
-        .then(res => {
+      $("#service-list").append(form); //#service-list is a <div> element in profile.html.
+
+      fetch("/courses")
+        .then((res) => {
           if (res.ok) {
             return res.json();
           } else {
             return null;
           }
         })
-        .then(res => {
+        .then((res) => {
           if (res) {
-            form.removeClass('invisible');
-            table.addClass('animate__animated animate__fadeInUpBig');
+            form.removeClass("invisible");
+            table.addClass("animate__animated animate__fadeInUpBig");
             for (let j = 0; j < services.length; j++) {
               const id = services[j];
-              const row = $('<tr></tr>');
-              table.append(row);//Create a row for each service.
+              const row = $("<tr></tr>");
+              table.append(row); //Create a row for each service.
               row.append(
-                $(`<td id="${id}-1"></td>`)
-                  .text(`${services[j].split('-').join(' ')}`)
-              );//Define the service title cell.
+                $(`<td id="${id}-1"></td>`).text(
+                  `${services[j].split("-").join(" ")}`,
+                ),
+              ); //Define the service title cell.
               row.append(
-                $(`<td id="${id}-2"></td>`)
-                  .html(`<input type="checkbox" name="${id}-sub" style="display: block; margin: auto">`) // Define the subsribe checkbox cell
-              )
-              const courseCell = $(`<td id=${id}-3></td>`);//Create the cource cell.            
-              const courseDiv = $('<div class="container-fluid"></div>');//Create the course div.
+                $(`<td id="${id}-2"></td>`).html(
+                  `<input type="checkbox" name="${id}-sub" style="display: block; margin: auto">`,
+                ), // Define the subsribe checkbox cell
+              );
+              const courseCell = $(`<td id=${id}-3></td>`); //Create the cource cell.
+              const courseDiv = $('<div class="container-fluid"></div>'); //Create the course div.
               courseCell.append(courseDiv);
               row.append(courseCell);
               if (types[j] == 1) {
                 listCourses(res, courseCell, courseDiv, services[j]);
-              } else if (types[j] == 2) {//Create the cross-reference cell.
+              } else if (types[j] == 2) {
+                //Create the cross-reference cell.
                 const targetCell = $(`<td id=${id}-4></td>`);
-                targetCell.append('<th>Target</th>');
-                courseCell.prepend('<th>Source</th>');
+                targetCell.append("<th>Target</th>");
+                courseCell.prepend("<th>Source</th>");
                 const targetDiv = $('<div class="container-fluid"></div>');
                 targetCell.append(targetDiv);
                 row.append(targetCell);
-                makeCourseMenu(res, courseDiv, services[j], '-source');
-                makeCourseMenu(res, targetDiv, services[j], '-target');
-                const addButton = $("<button type = 'button' class='btn btn-primary'>Add</button>");
-                addButton.on('click', function() {
-                  makeCourseMenu(res, courseDiv, services[j], '-source');
-                  makeCourseMenu(res, targetDiv, services[j], '-target');
+                makeCourseMenu(res, courseDiv, services[j], "-source");
+                makeCourseMenu(res, targetDiv, services[j], "-target");
+                const addButton = $(
+                  "<button type = 'button' class='btn btn-primary'>Add</button>",
+                );
+                addButton.on("click", function () {
+                  makeCourseMenu(res, courseDiv, services[j], "-source");
+                  makeCourseMenu(res, targetDiv, services[j], "-target");
                 });
                 targetCell.append(addButton);
               } else if (types[j] == 3) {
-                const groupCell = $(`<td id = ${id}-4></td>`)
-                const selectGroup = $('<select></select>');
-                selectGroup.attr('name', `${services[j]}-groups`);
+                const groupCell = $(`<td id = ${id}-4}></td>`);
+                const selectGroup = $("<select></select>");
+                selectGroup.attr("name", `${services[j]}-groups`);
                 groupCell.append(selectGroup);
                 row.append(groupCell);
-                const selectCourse = makeCourseMenu(res, courseDiv, services[j], '-courses');
+                const selectCourse = makeCourseMenu(
+                  res,
+                  courseDiv,
+                  services[j],
+                  "-courses",
+                );
                 const selectId = selectCourse.attr("id");
                 $(`#${selectId} > option`)
-                  .filter(function() {
+                  .filter(function () {
                     return this.value == "null";
-                  }).on('click', function() {
-                    selectGroup.empty();
                   })
+                  .on("click", function () {
+                    selectGroup.empty();
+                  });
                 $(`#${selectId} > option`)
-                  .filter(function() {
+                  .filter(function () {
                     return this.value != "null";
                   })
-                  .on('click', function(e) {
-                    fetch('/groups', {
+                  .on("click", function (e) {
+                    fetch("/groups", {
                       method: "POST",
                       headers: {
-                        'Content-Type': 'text/plain'
+                        "Content-Type": "text/plain",
                       },
-                      body: e.currentTarget.value
-                    }).then(response => response.json())
-                      .then(response => {
+                      body: e.currentTarget.value,
+                    })
+                      .then((response) => response.json())
+                      .then((response) => {
                         selectGroup.empty();
-                        selectGroup.append('<option value="null">Select a group</option>');
-                        response.forEach(group => {
-                          selectGroup.append($(`<option value=${group.id}>${group.name}</option>`));
+                        selectGroup.append(
+                          '<option value="null">Select a group</option>',
+                        );
+                        response.forEach((group) => {
+                          selectGroup.append(
+                            $(
+                              `<option value=${group.id}>${group.name}</option>`,
+                            ),
+                          );
                         });
                       });
                   });
               } else if (types[j] == 4) {
-                makeCourseMenu(res, courseDiv, services[j], '-course');
+                makeCourseMenu(res, courseDiv, services[j], "-course");
                 const infoCell = $(`<td id=${id}-4></td>`);
                 const infoDiv = $('<div class="container-fluid"></div>');
                 row.append(infoCell);
                 infoCell.append(infoDiv);
-                const options = ['name', 'group'];
+                const options = ["name", "group"];
                 const optionLen = options.length;
                 const labels = Array(optionLen).fill();
                 const inputs = Array(optionLen).fill();
-  
+
                 const ids = Array(optionLen).fill();
                 for (let q = 0; q < optionLen; q++) {
-                  const inputType = 'text';
-                  ids[q] = services[j] + '-' + options[q];
-                  labels[q] = $('<label></label>');
-                  labels[q]
-                    .text(options[q])
-                    .attr('for', ids[q])
-                  inputs[q] = $('<input></input>');
+                  const inputType = "text";
+                  ids[q] = services[j] + "-" + options[q];
+                  labels[q] = $("<label></label>");
+                  labels[q].text(options[q]).attr("for", ids[q]);
+                  inputs[q] = $("<input></input>");
                   inputs[q]
-                    .attr('id', ids[q])
-                    .attr('type', inputType)
-                    .attr('name', ids[q]);
-                  infoDiv
-                    .append(labels[q])
-                    .append(inputs[q]);
+                    .attr("id", ids[q])
+                    .attr("type", inputType)
+                    .attr("name", ids[q]);
+                  infoDiv.append(labels[q]).append(inputs[q]);
                 }
               }
-              const deliver = $(`<button type='submit' class='btn btn-primary' formaction='/${id}'>Deliver</button>`);              
+              const deliver = $(
+                `<button type='submit' class='btn btn-primary' formaction='/${id}'>Deliver</button>`,
+              );
               courseCell.append(deliver);
-            };
-          };
+            }
+          }
         });
       return;
     }
-  
+
     //Start populating from the database
-    const form = $('form');
-    
-    const table = $('table');
-    const profile = JSON.parse(sessionStorage.getItem('profile'));
+    const form = $("form");
+
+    const table = $("table");
+    const profile = JSON.parse(sessionStorage.getItem("profile"));
     const currServices = profile.services;
-    const populateCourses = function(serviceId) {
-      currServices[serviceId].courses.forEach(id => {
+    const populateCourses = function (serviceId) {
+      currServices[serviceId].courses.forEach((id) => {
         $(`#${serviceId}-3`)
-          .children('div')
+          .children("div")
           .children(`.${id}`)
-          .children('input')
-          .attr('checked', true);
+          .children("input")
+          .attr("checked", true);
       });
-    }
-  
+    };
+
     for (let service in currServices) {
       const serviceId = service;
       if (currServices[serviceId].active) {
-        $(`#${serviceId}-2`).children('input').attr('checked', true);
+        $(`#${serviceId}-2`).children("input").attr("checked", true);
         populateCourses(serviceId);
       }
-    };
-    form.append("<button id='subscribe_button' type='submit' class='btn btn-primary btn-block animate__animated animate__zoomIn' style='margin-top: 12pt; margin-bottom: 12pt'><i class='fas fa-paper-plane'></i> Submit</button>")
-    $('#subscribe_button').on('click', () => {
-      sessionStorage.setItem('submitted', 'true');
+    }
+    form.append(
+      "<button id='subscribe_button' type='submit' class='btn btn-primary btn-block animate__animated animate__zoomIn' style='margin-top: 12pt; margin-bottom: 12pt'><i class='fas fa-paper-plane'></i> Submit</button>",
+    );
+    $("#subscribe_button").on("click", () => {
+      sessionStorage.setItem("submitted", "true");
     });
   } else {
-    $('#service-list')
-      .text('The mobile device version is under construction. Please use a desktop device.')
-      .css('font-size', '16pt')
-      .append('<span class="glyphicon glyphicon-wrench" style="display: block; font-size: 150px; padding-top: 50px; text-align: center" aria-hidden="true"></span>');
+    $("#service-list")
+      .text(
+        "The mobile device version is under construction. Please use a desktop device.",
+      )
+      .css("font-size", "16pt")
+      .append(
+        '<span class="glyphicon glyphicon-wrench" style="display: block; font-size: 150px; padding-top: 50px; text-align: center" aria-hidden="true"></span>',
+      );
   }
-};
- 
+}
 
 //The subscription state cannot be displayed properly after submitting or refreshing.
-
